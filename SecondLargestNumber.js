a00fc53c833ffa52b490b74b50dd4450bebf09d4
ajima <ajimasaseendran@gmail.com>
2025-07-15 22:15:43 +0530

    if duplicates numbers are there, added one more condition for that

--- SecondLargestNumber.js
+++ SecondLargestNumber.js
@@ -1,4 +1,4 @@
-let arr = [1,3,4,2,5,3,45,3,43,34,232];
+let arr = [1,10,20,20];
 
 function findSecondLargestNumber(arr) {
     let largestNum=-Infinity;
@@ -9,7 +9,7 @@ function findSecondLargestNumber(arr) {
             //if we get the new largest number we will update the second largest with the previous largest
             secondLargestNum = largestNum;
             largestNum = arr[i]
-        }else if(arr[i] > secondLargestNum){
+        }else if(arr[i] > secondLargestNum && arr[i] != largestNum){
             // Update second largest if the current number is less than largest but greater than second largest
             secondLargestNum =arr[i] 
         }
@@ -19,3 +19,12 @@ function findSecondLargestNumber(arr) {
 
 let result = findSecondLargestNumber(arr);
 console.log(result);
+
+
+// corner cases of this solutions
+// 1. If the array has only one element, there is no second largest number.
+// 2. If all elements are the same, there is no second largest number.
+// 3. If the array is empty, there is no second largest number.
+// 4. If the array has negative numbers, the second largest number can also be negative
+// 5. If the array has duplicate elements, the second largest number should be unique.
+// 6. If the array has only two elements, the second largest number is the smaller of the two.
