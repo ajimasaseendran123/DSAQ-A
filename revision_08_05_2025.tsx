788b2d958b31bc7aea8c9bfeb3a2b9be4d97c83f
ajima <ajimasaseendran@gmail.com>
2025-08-05 14:20:16 +0530

    commit

---
+++ revision_08_05_2025.tsx
@@ -0,0 +1,42 @@
+how do we solve a DSA problem?
+1. solution
+2. edge cases
+3. optimization
+4. time complexity
+5. space complexity
+
+
+After considering all the above we start write the code.
+
+cornercases/edge cases are very important
+some edge cases are:
+what is array is empty
+what if the array have only one Element
+what if the array have only negative numbers
+what if the array have duplicates
+
+
+what is time complexity?
+measure efficiency of an algorithm in terms of speed as the input gorws
+low time complexity is better
+time complexity is not equal to time taken
+to represent the time complexity we use Big O notation
+big O notation -> measrures time complexity in the worst case.
+In the best case all the alogorithms will run in O(1) time complexity
+
+Linear search is O(n)
+Binary search is O(log n)
+nested loops are O(n^2)
+merge sort is O(n log n)
+
+space complexity
+means how much extra space that we are using 
+
+
+recursion
+function that calls itself to solve smaller versions of the same problem
+two parts of the recursion
+base case
+recursion case
+
+inifinte recursion will cause stack overflow
